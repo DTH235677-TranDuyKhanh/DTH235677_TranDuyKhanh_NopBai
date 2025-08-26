58aa3ca317ff57ac0229cdf0c1fd7d032a36366f
DTH235677-TranDuyKhanh <tranduykhanh240520@gmail.com>
2025-08-26 09:36:12 +0700

    Add files via upload

---
+++ chương_1.py
@@ -0,0 +1,10 @@
+import math
+import random
+# chương trình tính tổng hai số 
+# nhập hai số từ bàn phím 
+so1 = int(input ("Nhập số nguyên thứ nhất: "))
+so2 = int(input ("Nhập số nguyên thứ hai: "))
+# tính tổng 
+tong = so1 + so2 
+# hiện thị kết quả 
+print ("Kết quả là: ",tong)
